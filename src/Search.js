afc97cb97c1689989ac98157d6dd298cf9386232
Michael <jtmikee@gmail.com>
2021-03-16 15:27:01 -0400

    constant words and punctuation

--- src/Search.js
+++ src/Search.js
@@ -2,19 +2,19 @@ import axios from 'axios';
 import { Component } from 'react';
 
 
- //you have to call it like this when creating a constructor because thems the rules 
+//you have to call it like this when creating a constructor because thems the rules 
 class Search extends Component {
 
-    constructor () {
+    constructor() {
         //when using a constructor super is necessary because it allows us to use "this" 
         super();
 
 
         //state is initialized to create an array from the user input value
         this.state = {
-            
+
             userSearch: "",
-            
+
             chosenWord: [],
         }
     }
@@ -46,15 +46,15 @@ class Search extends Component {
         e.preventDefault()
         this.userSearch()
 
-        this.setState ({
+        this.setState({
             userSearch: ""
         })
     }
 
-     allowDrop = (e) => {
+    allowDrop = (e) => {
         e.preventDefault();
     }
-     drag = (e) => {
+    drag = (e) => {
         e.dataTransfer.setData("text/plain", e.target.id);
     }
     drop = (e) => {
@@ -62,14 +62,14 @@ class Search extends Component {
         // Get the data, which is the id of the drop target
         let data = e.dataTransfer.getData("text");
         e.target.append(document.getElementById(data));
-        
-       
+
+
     }
 
     // this function allows us to only call api on user search
     userSearch = () => {
-    
-            axios({
+
+        axios({
             method: "GET",
             url: "https://api.datamuse.com/words?",
             dataResponse: "JSON",
@@ -77,79 +77,108 @@ class Search extends Component {
                 topics: this.state.userSearch,
                 rel_bga: this.state.userSearch,
                 format: "JSON",
-                max: 10,
+                max: 50,
             },
-            }).then((res) => {
-                console.log(res)
-                const wordResults = res.data
-                console.log(wordResults)
+        }).then((res) => {
+            console.log(res)
+            const wordResults = res.data
+            console.log(wordResults)
 
-                const filteredWordResults = wordResults.filter((results) => {
-                    if 
-                    (results.word === 'who' || results.word === "what" || results.word === "when" || results.word === "where" || results.word === "why" || results.word === "how" || results.word === "the" || results.word === "a" || results.word === "is" || results.word === "he" || results.word === "his" || results.word === "she" || results.word === "her" || results.word === "hers" || results.word === "they" || results.word === "them" || results.word === "their" || results.word === "there" || results.word === "are" || results.word === "it" || results.word === "I" || results.word === "you" || results.word === "my" || results.word === "for" || results.word === "and" || results.word === "or" || results.word === "me" || results.word === "your" || results.word === "but" || results.word === "so" || results.word === "that" || results.word === "in" || results.word === "on" || results.word === "as" || results.word === "of" || results.word === "by" || results.word === "was" || results.word === "had" || results.word === "from" || results.word === "will" || results.word === "can" || results.word === "would" || results.word === "this" || results.word === "all" || results.word === "only" || results.word === "out" || results.word === "with" || results.word === "which" || results.word === "went" || results.word === "did" || results.word === "then" || results.word === "like"){
-                        results.word = null
-                    } else {
-                        return results.word;
-                    }
-                })
-                
-                const newState = []
-                
-                //creates our own array so you don't have to filter through all the json results
-                filteredWordResults.map( (results) => {
-                    return newState.push({
+            const filteredWordResults = wordResults.filter((results) => {
+                if
+                    (results.word === 'who' || results.word === "what" || results.word === "when" || results.word === "where" || results.word === "why" || results.word === "how" || results.word === "the" || results.word === "a" || results.word === "is" || results.word === "he" || results.word === "his" || results.word === "she" || results.word === "her" || results.word === "hers" || results.word === "they" || results.word === "them" || results.word === "their" || results.word === "there" || results.word === "are" || results.word === "it" || results.word === "I" || results.word === "you" || results.word === "my" || results.word === "for" || results.word === "and" || results.word === "or" || results.word === "me" || results.word === "your" || results.word === "but" || results.word === "so" || results.word === "that" || results.word === "in" || results.word === "on" || results.word === "as" || results.word === "of" || results.word === "by" || results.word === "was" || results.word === "had" || results.word === "from" || results.word === "will" || results.word === "can" || results.word === "would" || results.word === "this" || results.word === "all" || results.word === "only" || results.word === "out" || results.word === "with" || results.word === "which" || results.word === "went" || results.word === "did" || results.word === "then" || results.word === "like") {
+                    results.word = null
+                } else {
+                    return results.word;
+                }
+            })
+
+            const newState = []
+
+            //creates our own array so you don't have to filter through all the json results
+            filteredWordResults.map((results) => {
+                return newState.push({
                     word: results.word,
-                    })
-    
                 })
-                this.setState({
-                    chosenWord: newState,
-                })
-            }) 
 
-            
-        
+            })
+            this.setState({
+                chosenWord: newState,
+            })
+        })
+
+
+
+
+
     }
 
+
+
     render() {
+        const constantWords = [
+            "who", "what", "when", "where", "why", "how", "the", "a", "is", "he", "his", "she", "her", "hers", "they", "them", "their", "there", "are", "it", "I", "you", "my", "for", "and", "or", "me", "your", "but", "so", "that", "in", "on", "as", "of", "by", "was", "had", "from", "will", "can", "would", "this", "all", "only", "out", "with", "which", "went", "did", "then", "like"
+        ]
+        const constantPunctuation = [
+            ".", ",", ":", "!", "?"
+        ]
         return (
             <>
-        
-                <form onSubmit= {this.handleSubmit}>
+
+                <form onSubmit={this.handleSubmit}>
                     <label className="sr-only" htmlFor="search">Search</label>
                     <input type="text" placeholder="enter word here" id="search" value={this.state.userSearch} onChange={this.handleChange}></input>
                     <button type="submit" >Search</button>
                 </form>
 
                 <h2>Results</h2>
-                <div class="dropBox resultsArea" onDrop={this.drop} onDragOver={this.allowDrop} onDragStart={this.drag}>
-                    
+                <div className="dropBox resultsArea" onDrop={this.drop} onDragOver={this.allowDrop} onDragStart={this.drag}>
 
-                <ul >
-                    {
-                        this.state.chosenWord.map((results, index ) => {
-                        return (
-                            <li  id={index} onDragStart={this.drag}  draggable="true" key={index}>{results.word}</li>
-                                   
-                        )
-                        })
-                    }
-                </ul>
-            
+
+                    <ul >
+                        {
+                            this.state.chosenWord.map((results, index) => {
+                                return (
+                                    <li id={index} onDragStart={this.drag} draggable="true" key={index}>{results.word}</li>
+
+                                )
+                            })
+                        }
+                    </ul>
+
                 </div>
-                <br/>
+                <br />
                 <h2>Function and Suffixes</h2>
-                <div class="dropBox suffixArea" onDrop={this.drop} onDragOver={this.allowDrop} onDragStart={this.drag}></div>
+                <div className="dropBox suffixArea" onDrop={this.drop} onDragOver={this.allowDrop} onDragStart={this.drag}>
+                    <ul>
+                        {
+                            constantWords.map((word, index) => {
+                                return (
+                                    <li key={index}>{word}</li>
+                                )
+                            })
+                        }
+                    </ul>
+                    <ul>
+                        {
+                            constantPunctuation.map((word, index) => {
+                                return (
+                                    <li key={index}>{word}</li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
 
-                <br/>
+                <br />
                 <h2>Poem Area</h2>
-                <div class="dropBox poemArea" onDrop={this.drop} onDragOver={this.allowDrop} onDragStart={this.drag}></div>
+                <div className="dropBox poemArea" onDrop={this.drop} onDragOver={this.allowDrop} onDragStart={this.drag}></div>
             </>
-            
+
         )
 
     }
-    
+
 }
 
 export default Search;
