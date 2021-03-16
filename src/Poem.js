ff0767cea41c22ece726a36c296d1ee795b30d0a
Michael <jtmikee@gmail.com>
2021-03-16 17:34:23 -0400

    drag test

--- src/Poem.js
+++ src/Poem.js
@@ -36,15 +36,33 @@ const Poem = () => {
 	//form onsubmit handler 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		const databaseRef = firebase.database().ref();
-		databaseRef.push(finalPoem);
-		setFinalPoem("")
+		// const databaseRef = firebase.database().ref();
+		// databaseRef.push(finalPoem);
+		// setFinalPoem("")
+		console.log(event);
 	}
 
 	// const handleClick = (commentKey) => {
 	// 	const databaseRef = firebase.database().ref();
 	// 	databaseRef.child(commentKey).remove();
 	// }
+
+	const allowDrop = (e) => {
+        e.preventDefault();
+    }
+    const drag = (e) => {
+        e.dataTransfer.setData("text/plain", e.target.id);
+    }
+    const drop = (e) => {
+        e.preventDefault();
+        // Get the data, which is the id of the drop target
+        let data = e.dataTransfer.getData("text");
+        e.target.append(document.getElementById(data));
+    }
+
+	const handleClick = (event) => {
+		console.log(event)
+	}
 
 
 	return (
@@ -53,10 +71,10 @@ const Poem = () => {
 		<div>
 			<form onSubmit={handleSubmit}>
 				<label htmlFor="leavePoem">Displaying All Comments</label>
-				<div>
-					<textarea placeholder="Leave me a comment!" type="text" name="leavePoem" id="leavePoem" cols="30" rows="10" maxLength="200" required value={finalPoem} onChange={handleChange}></textarea>
-					<button className="addButton">Add Comment</button>
+				<div type="text" className="dropBox resultsArea" onDrop={drop} onDragOver={allowDrop} onDragStart={drag} onChange={handleChange} value={finalPoem} name="leavePoem" onClick={handleClick}>
+					{/* <textarea placeholder="Leave me a comment!" type="text" name="leavePoem" id="leavePoem" cols="30" rows="10" maxLength="200" required ></textarea> */}
 				</div>
+					<button className="addButton" onClick={handleClick}>Add Comment</button>
 			</form>
 			<ul>
 				{
@@ -70,6 +88,9 @@ const Poem = () => {
 				}
 
 			</ul>
+			<div className="testBox">
+
+			</div>
 		</div>
 
 	)
