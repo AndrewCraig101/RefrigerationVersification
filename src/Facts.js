bc8aaf01fa27a7db38ed470d32ecdacc59a78d25
Andrew Craig <56005072+AndrewCraig101@users.noreply.github.com>
2021-03-17 20:25:19 -0400

    adding final Facts.js

---
+++ src/Facts.js
@@ -0,0 +1,99 @@
+
+
+
+const poetryTerms1 = [
+
+    {
+        title: "Assonance",
+        definition: "Close repetition of vowel sounds."
+    },
+    {
+        title: "Alliteration",
+        "definition": "Close repetition of consonant sounds, especially initial consonant sounds."
+    },
+    {
+        title: "Caesura",
+        definition: "A deliberate rhetorical, grammatical, or rhythmic pause, break, cut, turn, division, or pivot in poetry."
+    },
+    {
+        title: "Chapbook",
+        definition: "A small book of about 24-50 pages."
+    },
+    {
+        title: "Epigraph",
+        definition: "A short verse, note, or quotation that appears at the beginning of a poem or section; usually presents an idea or theme on which the poem elaborates, or contributes background information not reflected in the poem itself."
+    },
+    {
+        title: "Enjambment",
+        definition: "Continuation of sense and rhythmic movement from one line to the next; also called a run on line."
+    }
+]
+
+const poetryTerms2 = [
+
+    {
+        title: "Meter",
+        definition: "The rhythmic measure of a line."
+    },
+    {
+        title: "Refrain",
+        definition: "A repeated line within a poem, similar to the chorus of a song."
+    },
+    {
+        title: "Stanza",
+        definition: "Group of lines making up a single unit; like a paragraph in prose."
+    },
+    {
+        title: "Tercet",
+        definition: "Stanza or poem of 3 lines."
+    },
+    {
+        title: "Trochee",
+        definition: "Foot consisting of a stress followed by an unstressed syllable."
+    },
+    {
+        title: "Line",
+        definition: "Basic unit of a poem; measured in feet if metrical."
+    }
+]
+
+
+const Facts = () => {
+
+    const random_index = Math.floor(Math.random() * 6)
+
+    const poemProfile1 = poetryTerms1[random_index];
+
+    const poemProfile2 = poetryTerms2[random_index];
+
+    console.log(poemProfile2)
+    console.log(poemProfile1)
+
+
+    return (
+        <>
+            <div className="factWrapper" >
+                <h1 className="poetryHeading">Poetry Facts!</h1>
+                <section className="poetryDivSection" >
+                    <div className="poetryTermsDiv poetryTerms1div">
+                        <p className="titlePara">{poemProfile1.title}</p>
+                        <p className="titleDef">{poemProfile1.definition}</p>
+                        <p className="linkPara">From <a href="https://quizlet.com" className="factsLink">quizlet.com</a></p>
+                    </div>
+
+                    <div className="poetryTermsDiv poetryTerms2div">
+                        <p className="titlePara">{poemProfile2.title}</p>
+                        <p className="titleDef">{poemProfile2.definition}</p>
+                        <p className="linkPara">From <a href="https://www.writersdigest.com" className="factsLink">writersdigest.com</a></p>
+                    </div>
+                </section >
+            </div>
+        </>
+
+    )
+
+}
+
+export default Facts
+
+
